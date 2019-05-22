fa6f0ec75c3e14bc296937c028e4b840c7674e2e
Raúl Berganza <brgraul@gmail.com>
2019-05-22 11:55:36 +0200

    Deploying my awesome vue app

---
+++ precache-manifest.3b0f3e72b1c97365ae764255810f56d6.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "c4e798052911f915083d",
+    "url": "/<taskprize>/css/about.c31a8717.css"
+  },
+  {
+    "revision": "c4e798052911f915083d",
+    "url": "/<taskprize>/js/about.ce8442a1.js"
+  },
+  {
+    "revision": "28dc9ea61796413c1e0e",
+    "url": "/<taskprize>/css/app.a1338d2f.css"
+  },
+  {
+    "revision": "28dc9ea61796413c1e0e",
+    "url": "/<taskprize>/js/app.f6a74091.js"
+  },
+  {
+    "revision": "29809448ba6b180edbe8",
+    "url": "/<taskprize>/css/chunk-vendors.af9df320.css"
+  },
+  {
+    "revision": "29809448ba6b180edbe8",
+    "url": "/<taskprize>/js/chunk-vendors.84f4f963.js"
+  },
+  {
+    "revision": "aeee9d8ff80747c5a2620bdd871c6e7a",
+    "url": "/<taskprize>/index.html"
+  },
+  {
+    "revision": "e11bf1ee682968f00680983c8c913c32",
+    "url": "/<taskprize>/bundle.js"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/<taskprize>/robots.txt"
+  }
+];
