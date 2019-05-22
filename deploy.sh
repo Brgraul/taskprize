9cbaec741b3c579e90a5bd6ff77ddff0e7e45a3f
Raúl Berganza <brgraul@gmail.com>
2019-05-22 11:10:00 +0200

    Deployment changes

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:siebenrock/smart-microtasks.git master:gh-pages
+git push -f git@github.com:Brgraul/taskprize.git master:gh-pages
 
 cd -
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
 module.exports = {
-  publicPath:
-    process.env.NODE_ENV === "production" ? "/smart-microtasks/" : "/",
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/taskprize/'
+    : '/'
 };
